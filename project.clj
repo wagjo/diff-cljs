14bc2b416486f17c58e5a7405f6945d15c9098cb
Jozef Wagner <jozef.wagner@gmail.com>
2013-01-25 22:53:49 +0100

    add dependency to wagjo/data-cljs

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Diff for ClojureScript Sequences."
   :url "https://github.com/wagjo/cljs-diff"
   :source-paths ["src/cljs"]
+  :dependencies [[com.wagjo/data-cljs "0.1.0-SNAPSHOT"]]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
