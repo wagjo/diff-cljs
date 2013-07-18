c7fbecdcd0aabb1936922aa0e1aa38fb243e784e
Jozef Wagner <jozef.wagner@gmail.com>
2013-07-18 23:16:37 +0200

    rename repo name

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject com.wagjo/cljs-diff "0.1.0-SNAPSHOT"
-  :description "Diff for ClojureScript Sequences."
-  :url "https://github.com/wagjo/cljs-diff"
+(defproject com.wagjo/diff-cljs "0.1.0-SNAPSHOT"
+  :description "Diff for ClojureScript sequences."
+  :url "https://github.com/wagjo/diff-cljs"
   :source-paths ["src/cljs"]
   :dependencies [[com.wagjo/data-cljs "0.1.0-SNAPSHOT"]]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
-            :comments "same as ClojureScript"}
-  :warn-on-reflection true)
+            :comments "same as ClojureScript"})
